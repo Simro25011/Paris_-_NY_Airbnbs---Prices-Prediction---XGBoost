c99642d3c9b493c3c9963760db5fd5f92460c71d
Arnold Tchuente <108312511+Simro25011@users.noreply.github.com>
2023-05-15 21:14:31 -0400

    Add files via upload
    adding prices predictions and setting now for vizualizations

---
+++ Airbnb_join_query.sql
@@ -0,0 +1,11 @@
+select * from paris_airbnbs 
+select * from nyc_clean_airbnb
+select * from paris_clean_airbnb
+
+ALTER TABLE paris_clean_airbnb 
+ALTER COLUMN "ID" TYPE text;
+
+CREATE TABLE paris_nyc_airbnbs AS
+SELECT * FROM paris_clean_airbnb
+UNION ALL
+SELECT * FROM nyc_clean_airbnb;
